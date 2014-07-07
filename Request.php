e8a555e982bee91a9205aa8559f775de0cbcf2d3
Phan Thanh Cong <ptcong90@gmail.com>
2014-07-07 16:03:07 +0700

    Missing directory separator

--- Request.php
+++ Request.php
@@ -1,5 +1,5 @@
 <?php
 
-require_once dirname(__FILE__) . 'Client.php';
+require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'Client.php';
 
 class ChipVN_Http_Request extends ChipVN_Http_Client {}
